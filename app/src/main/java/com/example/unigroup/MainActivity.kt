8dafac97591909d9882e2362c8c65cbdbe063878
Tsoy <maratts12342334@gmail.com>
2026-02-20 15:16:39 +0300

    Create login screen and migrate Login screen to MVVM using viewModel

--- app/src/main/java/com/example/unigroup/MainActivity.kt
+++ app/src/main/java/com/example/unigroup/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.unigroup
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UniGroupTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+               Surface(
+                   modifier = Modifier.fillMaxSize(),
+                   color = MaterialTheme.colorScheme.background
+               ){
+                   LoginScreen()
                 }
             }
         }
